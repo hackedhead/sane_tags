227729db77efd3e9a3700a1ba4fe683501bd9945
hackedhead <hackedhead@users.noreply.github.com>
2014-08-07 09:27:37 -0400

    package for setuptools

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+long_description = open('README.md').read()
+
+setup(
+    name="sane_tags",
+    version="0.1",
+    description="A method for tagging in Django without using GenericForiegnKeys",
+    long_description=long_description,
+    maintainer="hackedhead",
+    maintainer_email="hackedhead@gmail.com",
+    url="https://github.com/hackedhead/sane_tags"
+    )
